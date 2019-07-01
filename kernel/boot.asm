e8a3f989690cd004457db85892fe142160d8c6df
chengzhang90 <877982058@qq.com>
2019-07-01 15:56:35 +0800

    Create boot.asm
    定义了GRUB启动需要的域MBOOT_MAGIC、MBOOT_FLAGS和MBOOT_CHECKSUM，并调用了内核入口函数kernel_main

---
+++ kernel/boot.asm
@@ -0,0 +1,21 @@
+;flie name:boot.asm
+
+MBOOT_MAGIC equ 0x1BADB002
+MBOOT_FLAGS equ 0x00
+MBOOT_CHECKSUM equ -(MBOOT_MAGIC + MBOOT_FLAGS)
+
+[BITS 32]
+
+section .text
+  dd MBOOT_MAGIC
+  dd MBOOT_FLAGS
+  dd MBOOT_CHECKSUM
+  dd start
+
+[GLOBAL start]
+[EXTERN kernel_main]
+
+start:
+  cli
+  call kernel_main
+  jmp $
